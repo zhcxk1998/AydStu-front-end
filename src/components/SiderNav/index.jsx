4c51755c41df2ae3d42da5c5ee02342fb9ad5ace
zhcxk1998 <464203147@qq.com>
2019-10-03 00:12:14 +0800

    feat: 新增首页侧边栏.

---
+++ src/components/SiderNav/index.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import {
+  Menu, Icon,
+} from 'antd';
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const { SubMenu } = Menu;
+
+export default class SiderNav extends React.Component {
+  render() {
+    const { collapsed } = this.props
+
+    return (
+      <div>
+        {!!!collapsed ? (
+          <div className="nav-container">
+            <div className="nav-header">
+              <img src="http://cdn.algbb.cn/read.png" alt="" />
+              <div>爱阅读学生端</div>
+            </div>
+            <div className="nav-info">
+              <img src="http://cdn.algbb.cn/emoji/32.png" alt="" />
+              <div className="nickname">Tom Richard</div>
+              <div className="description">A student of BNUZ.</div>
+            </div>
+          </div>
+        ) : (
+          <img className="logo" src="http://cdn.algbb.cn/read.png" alt="" />
+        )}
+
+        <Menu defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">
+            <Link to="/login">
+              <Icon type="pie-chart" />
+              <span>个人中心</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="read" />
+            <span>图书管理</span>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={(
+              <span>
+                <Icon type="shopping-cart" />
+                <span>海洋世界</span>
+              </span>
+            )}
+          >
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={(
+              <span>
+                <Icon type="apartment" />
+                <span>作品社区</span>
+              </span>
+            )}
+          >
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9">
+            <Icon type="play-square" />
+            <span>名师微课</span>
+          </Menu.Item>
+          <Menu.Item key="10">
+            <Icon type="edit" />
+            <span>书香奇谈</span>
+          </Menu.Item>
+          <Menu.Item key="11">
+            <Icon type="carry-out" />
+            <span>阅读任务</span>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { observer, inject } from 'mobx-react'
+
+import {
+  Layout, Menu, Icon,
+} from 'antd';
+import SiderNav from '../../components/SiderNav'
 
 import './index.scss'
 
-@observer
-@inject('test')
+const {
+  Content, Sider, Footer,
+} = Layout;
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: false,
+    }
+  }
+
+  onCollapse = () => {
+    this.setState((preState) => ({
+      collapsed: !preState.collapsed,
+    }))
+  }
+
   render() {
-    const { test } = this.props
+    const { collapsed } = this.state
     return (
-      <div>{test.count}</div>
+      <Layout hasSider>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} theme="light" className="slider">
+          <SiderNav collapsed={collapsed} />
+        </Sider>
+        <Layout className={`${collapsed ? 'layout-collapsed' : 'layout-unfold'}`}>
+          <Content>
+            {/* <ContentMain /> */}
+            {Array.from({ length: 100 }).map((item) => (
+              <div>1</div>
+            ))}
+          </Content>
+          <Footer>
+            <div>Footer</div>
+          </Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
