1d96b6c7afc411625039e2e5793465df41efc2f9
zhcxk1998 <464203147@qq.com>
2019-10-08 20:26:22 +0800

    style: 美化登录界面的样式.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -58,71 +58,74 @@ class LoginForm extends React.Component {
 
     return (
       <div className="login-container">
-        <div className="logo-container" />
-        <Form onSubmit={this.handleSubmit} className="form-container">
-          <Form.Item>
-            {getFieldDecorator('school', {
-              rules: [
-                { required: true, message: '请选择学校信息' },
-              ],
-            })(
-              <Select
-                placeholder="请选择您的学校"
-                labelInValue
-              >
-                <Option value="bnuz">北京师范大学珠海分校</Option>
-                <Option value="bitz">北京理工大学珠海分校</Option>
-                <Option value="jluz">吉林大学珠海学院</Option>
-              </Select>,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('type', {
-              rules: [
-                { required: true, message: '请选择登录类型!' },
-              ],
-            })(
-              <Select
-                placeholder="请选择登录类型"
-              >
-                <Option value="teacher">教师</Option>
-                <Option value="student">学生</Option>
-                <Option value="admin">管理员</Option>
-              </Select>,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: '请输入用户名!', whitespace: true }],
-            })(<Input autoComplete="off" placeholder="请输入用户名" />)}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入密码!',
-                },
-              ],
-            })(<Input.Password autoComplete="off" placeholder="请输入密码" />)}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              initialValue: true,
-              valuePropName: 'checked',
-            })(
-              <Checkbox>
-                记住密码
-              </Checkbox>,
-            )}
-            <Link style={{ float: 'right' }} to="/register">点击这里，立即注册</Link>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
+        <div className="login-wrap">
+          <div className="logo-container" />
+          <Form onSubmit={this.handleSubmit} className="form-container">
+            <Form.Item>
+              {getFieldDecorator('school', {
+                rules: [
+                  { required: true, message: '请选择学校信息' },
+                ],
+              })(
+                <Select
+                  placeholder="请选择您的学校"
+                  labelInValue
+                >
+                  <Option value="bnuz">北京师范大学珠海分校</Option>
+                  <Option value="bitz">北京理工大学珠海分校</Option>
+                  <Option value="jluz">吉林大学珠海学院</Option>
+                </Select>,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('type', {
+                rules: [
+                  { required: true, message: '请选择登录类型!' },
+                ],
+              })(
+                <Select
+                  placeholder="请选择登录类型"
+                >
+                  <Option value="teacher">教师</Option>
+                  <Option value="student">学生</Option>
+                  <Option value="admin">管理员</Option>
+                </Select>,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: '请输入用户名!', whitespace: true }],
+              })(<Input autoComplete="off" placeholder="请输入用户名" />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入密码!',
+                  },
+                ],
+              })(<Input.Password autoComplete="off" placeholder="请输入密码" />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                initialValue: true,
+                valuePropName: 'checked',
+              })(
+                <Checkbox>
+                  记住密码
+                </Checkbox>,
+              )}
+              <Link style={{ float: 'right' }} to="/register">点击这里，立即注册</Link>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+
       </div>
     );
   }
