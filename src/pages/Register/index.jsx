483652d3655fa27c496cf5daada40a2fece7facb
zhcxk1998 <464203147@qq.com>
2019-10-08 20:29:21 +0800

    style: 更新注册界面样式.

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -64,7 +64,7 @@ class RegisterForm extends React.Component {
     callback();
   };
 
-  validatevalidateRegister=(params = {}) => {
+  validatevalidateRegister = (params = {}) => {
     http.put('http://localhost:4000/users/register', params)
   }
 
@@ -74,79 +74,81 @@ class RegisterForm extends React.Component {
 
     return (
       <div className="register-container">
-        <div className="logo-container" />
-        <Form onSubmit={this.handleSubmit} className="form-container">
-          <Form.Item>
-            {getFieldDecorator('school', {
-              rules: [
-                { required: true, message: '请选择学校信息' },
-              ],
-            })(
-              <Select
-                placeholder="请选择您的学校"
-                labelInValue
-              >
-                <Option value="bnuz">北京师范大学珠海分校</Option>
-                <Option value="bitz">北京理工大学珠海分校</Option>
-                <Option value="jluz">吉林大学珠海学院</Option>
-              </Select>,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('type', {
-              rules: [
-                { required: true, message: '请选择登录类型!' },
-              ],
-            })(
-              <Select
-                placeholder="请选择登录类型"
-              >
-                <Option value="teacher">教师</Option>
-                <Option value="student">学生</Option>
-                <Option value="admin">管理员</Option>
-              </Select>,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: '请输入用户名!', whitespace: true }],
-            })(<Input autoComplete="off" placeholder="请输入用户名" />)}
-          </Form.Item>
-          <Form.Item hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入密码!',
-                },
-                {
-                  validator: this.validateToNextPassword,
-                },
-              ],
-            })(<Input.Password placeholder="请输入密码" />)}
-          </Form.Item>
-          <Form.Item hasFeedback>
-            {getFieldDecorator('confirm', {
-              rules: [
-                {
-                  required: true,
-                  message: '请确认密码!',
-                },
-                {
-                  validator: this.compareToFirstPassword,
-                },
-              ],
-            })(<Input.Password onBlur={this.handleConfirmBlur} placeholder="请确认密码" />)}
-          </Form.Item>
-          <Form.Item>
-            <Link style={{ float: 'right' }} to="/login">已有用户？立即登录</Link>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              注册
-            </Button>
-          </Form.Item>
-        </Form>
+        <div className="register-wrap">
+          <div className="logo-container" />
+          <Form onSubmit={this.handleSubmit} className="form-container">
+            <Form.Item>
+              {getFieldDecorator('school', {
+                rules: [
+                  { required: true, message: '请选择学校信息' },
+                ],
+              })(
+                <Select
+                  placeholder="请选择您的学校"
+                  labelInValue
+                >
+                  <Option value="bnuz">北京师范大学珠海分校</Option>
+                  <Option value="bitz">北京理工大学珠海分校</Option>
+                  <Option value="jluz">吉林大学珠海学院</Option>
+                </Select>,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('type', {
+                rules: [
+                  { required: true, message: '请选择登录类型!' },
+                ],
+              })(
+                <Select
+                  placeholder="请选择登录类型"
+                >
+                  <Option value="teacher">教师</Option>
+                  <Option value="student">学生</Option>
+                  <Option value="admin">管理员</Option>
+                </Select>,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: '请输入用户名!', whitespace: true }],
+              })(<Input autoComplete="off" placeholder="请输入用户名" />)}
+            </Form.Item>
+            <Form.Item hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入密码!',
+                  },
+                  {
+                    validator: this.validateToNextPassword,
+                  },
+                ],
+              })(<Input.Password placeholder="请输入密码" />)}
+            </Form.Item>
+            <Form.Item hasFeedback>
+              {getFieldDecorator('confirm', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请确认密码!',
+                  },
+                  {
+                    validator: this.compareToFirstPassword,
+                  },
+                ],
+              })(<Input.Password onBlur={this.handleConfirmBlur} placeholder="请确认密码" />)}
+            </Form.Item>
+            <Form.Item>
+              <Link style={{ float: 'right' }} to="/login">已有用户？立即登录</Link>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                注册
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     );
   }
