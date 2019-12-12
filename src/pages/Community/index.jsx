32d139dc6180d8f57256e863584cb3db66fc9592
KonoYumDa <747495682@qq.com>
2019-12-12 23:29:20 +0800

    feat: 修改作品社区页面样式

--- src/pages/Community/index.jsx
+++ src/pages/Community/index.jsx
@@ -11,6 +11,8 @@ import {
   Drawer,
   Button,
   Collapse,
+  notification,
+  Pagination,
 } from 'antd';
 import './index.scss'
 import Meta from 'antd/es/card/Meta';
@@ -19,10 +21,78 @@ import RadioGroup from 'antd/es/radio/group';
 const { Panel } = Collapse;
 const { Search } = Input;
 
-const data = [
+const AllBook = [
   {
-    title: 'Title 1',
-    cover: <img alt="example" src="http://cdn.algbb.cn/emoji/32.png" />,
+    bookName: '安徒生童话',
+    author: '（丹麦）安徒生 ',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="https://ro.bnuz.edu.cn/book/category_tonghua/7f20b155-cf83-4d42-93e8-95310975afe7.png"
+    />,
+  },
+  {
+    bookName: '了不起的狐狸爸爸',
+    author: '（美国）罗尔德·达尔',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="http://ro.bnuz.edu.cn/book/category_tonghua/5917bd81-2daa-4cef-a940-9fec3a070f8c.png"
+    />,
+  },
+  {
+    bookName: '亲爱的汉修先生',
+    author: '贝芙莉·克莱瑞',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="http://ro.bnuz.edu.cn/book/category_xiaoshuo/0251d4d0-cdeb-446a-85ae-3f69010a7c8a.png"
+    />,
+  },
+  {
+    bookName: '鲁宾逊漂流记',
+    author: '（英国）笛福/著',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="https://ro.bnuz.edu.cn/book/category_xiaoshuo/06856e86-bdf7-4e9b-94e9-1a15b383a726.png"
+    />,
+  },
+  {
+    bookName: '夏洛的网',
+    author: 'E.B.怀特/E.B.White',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="http://ro.bnuz.edu.cn/book/category_tonghua/372e601c-a142-44b5-9b6f-98f841123229.png"
+    />,
+  },
+  {
+    bookName: '小猪唏哩呼噜',
+    author: '孙幼军 著',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="https://ro.bnuz.edu.cn/book/category_manhua/9cf63065-8082-4080-96f0-ea0859fdeb73.png"
+    />,
+  },
+  {
+    bookName: '草房子',
+    author: '曹文轩/ 著',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="http://ro.bnuz.edu.cn/book/category_xiaoshuo/026a0c79-7ef0-4ee9-8957-eadc50b1300a.png"
+    />,
+  },
+  {
+    bookName: '绿野仙踪',
+    author: '（美）弗兰克•鲍姆 著',
+    cover: <img
+      alt="example"
+      style={{ height: 282.4 }}
+      src="http://ro.bnuz.edu.cn/book/category_tonghua/1cea7c14-fd4f-4faa-907d-dc2cb100ea0e.png"
+    />,
   },
 ];
 const book = [
@@ -90,6 +160,18 @@ class Community extends React.Component {
     });
   };
 
+  onSearchBook = (type, value) => {
+    if (value === '') {
+      notification[type]({
+        message: '提示',
+        description:
+          '搜索的书名不能为空',
+      });
+    } else {
+      console.log(value)
+    }
+  };
+
 
   render() {
     return (
@@ -105,25 +187,24 @@ class Community extends React.Component {
             title="返回"
             subTitle="回到上一页"
             extra={[
-              <Search placeholder="输入书名" onSearch={(value) => console.log(value)} enterButton />,
+              <Search
+                id="findBook"
+                placeholder="输入书名"
+                size="large"
+                style={{ width: 500 }}
+                onSearch={(value) => this.onSearchBook('warning', value)}
+                enterButton
+              />,
             ]}
-          >
-            <Row>
-              <div style={{ marginTop: 20 }}>
-                <RadioGroup defaultValue="a" className="content-switch">
-                  <Radio.Button value="recommend">推荐书籍</Radio.Button>
-                  <Radio.Button value="all">全部书籍</Radio.Button>
-                </RadioGroup>
-              </div>
-            </Row>
-            <Descriptions size="default" column={3}>
-              <Descriptions.Item label="hello world">卖竹鼠，三个BB一只，一个BB三只</Descriptions.Item>
-            </Descriptions>
-          </PageHeader>
+          />
+          <div style={{ textAlign: 'center', marginRight: 90 }} className="wrap">
+            <RadioGroup defaultValue="all" className="content-switch" buttonStyle="solid">
+              <Radio.Button value="recommend" style={{ width: 300, height: 50, paddingTop: 7 }}>推荐书籍</Radio.Button>
+              <Radio.Button value="all" style={{ width: 300, height: 50, paddingTop: 7 }}>全部书籍</Radio.Button>
+            </RadioGroup>
+          </div>
         </div>
-        /**
-        *主界面
-        */
+        {/** 主界面 */}
         <div id="mainframe">
           <List
             className="content-mainframe"
@@ -136,21 +217,31 @@ class Community extends React.Component {
               xl: 3,
               xxl: 4,
             }}
-            dataSource={data}
+            dataSource={AllBook}
             renderItem={(item) => (
               <List.Item>
                 <Card
                   hoverable
                   className="data"
-                  actions={[<Icon type="ellipsis" key="ellipsis" onClick={this.showRightDraw} />]}
-                  style={{ width: 240 }}
+                  actions={[<Icon type="info-circle" key="ellipsis" onClick={this.showRightDraw} />]}
+                  style={{
+                    width: 280,
+                    marginTop: 30,
+                  }}
                   cover={item.cover}
                 >
-                  <Meta title="书名" description="作者" />
+                  <Meta title={item.bookName} description={item.author} />
                 </Card>
               </List.Item>
             )}
           />
+          <div style={{
+            textAlign: 'center', zIndex: 100, position: 'relative', right: 80,
+          }}
+          >
+            {/* 先弄100数据来测试样子 */}
+            <Pagination defaultCurrent={5} total={100} defaultPageSize={8} />
+          </div>
         </div>
         <Drawer
           id="rightDraw"
@@ -190,7 +281,13 @@ class Community extends React.Component {
                           </div>
                         </div>
                       </div>
-                      <div className="" style={{ display: 'inline-block', float: 'right' }}>
+                      <div
+                        className=""
+                        style={{
+                          display: 'inline-block',
+                          float: 'right',
+                        }}
+                      >
                         <div className="">
                           点赞数:
                           {likeCount}
@@ -223,7 +320,13 @@ class Community extends React.Component {
         >
           {comment.map((item, index) => (
             <Collapse accordion>
-              <Panel key={index} header={`发布人:${item.author}`}>{item.description}</Panel>
+              <Panel
+                className="shuping"
+                key={index}
+                header={`发布人:${item.author}`}
+              >
+                {item.description}
+              </Panel>
             </Collapse>
           ))}
         </Drawer>
